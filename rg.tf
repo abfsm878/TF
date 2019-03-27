055d8f125f618a025630d055d336038a40b48373
Abdel <35078911+abfsm878@users.noreply.github.com>
2019-03-27 15:29:30 -0600

    Add files via upload

--- rg.tf
+++ rg.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "test" {
-  name     = "acceptanceTestResourceGroup1"
-  location = "West US"
+  name     = "Terraform-RG"
+  location = "Canada Central"
 }
 
 resource "azurerm_network_security_group" "test" {
-  name                = "acceptanceTestSecurityGroup1"
+  name                = "NSG1"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 }
@@ -16,11 +16,11 @@ resource "azurerm_ddos_protection_plan" "test" {
 }
 
 resource "azurerm_virtual_network" "test" {
-  name                = "virtualNetwork1"
+  name                = "vNet1"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["192.168.0.0/16"]
+  dns_servers         = ["192.168.0.4", "192.168.0.5"]
 
   ddos_protection_plan {
     id     = "${azurerm_ddos_protection_plan.test.id}"
@@ -28,22 +28,22 @@ resource "azurerm_virtual_network" "test" {
   }
 
   subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
+    name           = "Subscription-Subnet"
+    address_prefix = "192.168.1.0/27"
   }
 
   subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
+    name           = "Cluster-Subnet"
+    address_prefix = "192.168.2.0/27"
   }
 
   subnet {
-    name           = "subnet3"
-    address_prefix = "10.0.3.0/24"
+    name           = "DMZ-Subnet"
+    address_prefix = "192.168.3.0/27"
     security_group = "${azurerm_network_security_group.test.id}"
   }
 
   tags = {
-    environment = "Production"
+    environment = "Terraform First Deployment"
   }
 }
